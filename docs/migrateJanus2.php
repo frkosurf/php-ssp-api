bb28686ac6912cede7f95e8a4d040f03084fafc9
François Kooman <fkooman@tuxed.net>
2013-03-12 20:50:24 +0100

    update migrate2 script

--- docs/migrateJanus2.php
+++ docs/migrateJanus2.php
@@ -10,20 +10,41 @@ $data = array();
 $pdo = new PDO("mysql:host=127.0.0.1;dbname=sr", "root", NULL);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// fetch all entries and their latest revision
-$sql = "SELECT metadataurl, arp, eid, type, entityid, state, revisionid FROM janus__entity e WHERE revisionid = (SELECT MAX(revisionid) FROM janus__entity WHERE eid = e.eid)";
+$sql = <<< EOF
+    SELECT
+        metadataurl, arp, eid, type, entityid, state, revisionid
+    FROM
+        janus__entity e
+    WHERE
+        revisionid = (SELECT
+                MAX(revisionid)
+            FROM
+                janus__entity
+            WHERE
+                eid = e.eid)
+EOF;
+
 $sth = $pdo->prepare($sql);
 $sth->execute();
 $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-$saml20_idp_remote = array();
-$saml20_sp_remote = array();
+$entities = array();
+$idpCount = 0;
+$spCount = 0;
 
 // for every entry fetch the metadata
 foreach ($result as $r) {
     $metadata = array();
 
-    $sql = "SELECT `key`, `value` FROM janus__metadata WHERE eid = :eid AND revisionid = :revisionid";
+$sql = <<< EOF
+    SELECT
+        `key`, `value`
+    FROM
+        janus__metadata
+    WHERE
+        eid = :eid AND revisionid = :revisionid
+EOF;
+
     $sth = $pdo->prepare($sql);
     $sth->bindValue(":eid", $r['eid']);
     $sth->bindValue(":revisionid", $r['revisionid']);
@@ -50,6 +71,29 @@ foreach ($result as $r) {
         }
     }
 
+$sql = <<< EOF
+    SELECT
+        e.entityid
+    FROM
+        `janus__entity` e,
+        `janus__allowedEntity` a
+    WHERE
+        a.eid = :eid AND a.revisionid = :revisionid
+            AND e.eid = a.remoteeid
+            AND e.revisionid = (SELECT
+                MAX(revisionid)
+            FROM
+                `janus__entity`
+            WHERE
+                eid = a.remoteeid)
+EOF;
+
+    $sth = $pdo->prepare($sql);
+    $sth->bindValue(":eid", $r['eid']);
+    $sth->bindValue(":revisionid", $r['revisionid']);
+    $sth->execute();
+    $a = $sth->fetchAll(PDO::FETCH_COLUMN);
+
     $metadata['entityid'] = $r['entityid'];
     if (!empty($r['metadataurl'])) {
         $metadata['metadata-url'] = $r['metadataurl'];
@@ -57,21 +101,28 @@ foreach ($result as $r) {
     $metadata['metadata-set'] = $r['type'] . "-remote";
     $metadata['state'] = $r['state'];
 
-    // FIXME: add ACL
-
     if ($metadata['metadata-set'] === "saml20-sp-remote") {
-        array_push($saml20_sp_remote, $metadata);
+        $metadata['IDPList'] = $a;
+        $spCount++;
+        array_push($entities, $metadata);
     } elseif ($metadata['metadata-set'] === "saml20-idp-remote") {
-        array_push($saml20_idp_remote, $metadata);
+        $metadata['SPList'] = $a;
+        $idpCount++;
+        array_push($entities, $metadata);
     } else {
         throw new Exception("unsupported entity type");
     }
 }
 
-echo count($saml20_idp_remote) . " IdPs" . PHP_EOL;
-echo count($saml20_sp_remote) . " SPs" . PHP_EOL;
+echo $idpCount . " IdPs" . PHP_EOL;
+echo $spCount . " SPs" . PHP_EOL;
 
-file_put_contents($argv[1], json_encode($saml20_idp_remote + $saml20_sp_remote));
+$idpWithACL = findIdPWithACL($entities);
+$spWithACL = findSPWithACL($entities);
+
+findConflictingACL($idpWithACL, $spWithACL);
+
+file_put_contents($argv[1], json_encode($entities));
 
 function arrayizeMetadata(&$metadata)
 {
@@ -98,5 +149,49 @@ function arrayizeMetadata(&$metadata)
             }
         }
     }
+}
 
+function findIdPWithACL(&$entities)
+{
+    $idpWithACL = array();
+    foreach ($entities as $e) {
+        if (array_key_exists("SPList", $e) && 0 !== count($e['SPList'])) {
+            $idpWithACL[$e['entityid']] = $e['SPList'];
+        }
+    }
+
+    return $idpWithACL;
+}
+
+function findSPWithACL(&$entities)
+{
+    $spWithACL = array();
+    foreach ($entities as $e) {
+        if (array_key_exists("IDPList", $e) && 0 !== count($e['IDPList'])) {
+            $spWithACL[$e['entityid']] = $e['IDPList'];
+        }
+    }
+
+    return $spWithACL;
+}
+
+function findConflictingACL(&$idpWithACL, &$spWithACL)
+{
+    // the ACL at the SP MUST match with the ACL at the IdP
+    //
+    // if there is an ACL at the SP:
+    // 1. the IdP entities mentioned MUST have this SP entityId in the SPList
+    // 2. the other IdP entities MUST NOT have this SP entityId in the SPList
+
+    foreach ($spWithACL as $eid => $list) {
+        foreach ($list as $l) {
+            if (!array_key_exists($l, $idpWithACL)) {
+                echo "[WARNING] mentioned IdP '" . $l . "' at SP '" . $eid . "' does not exist, or has no ACL" . PHP_EOL;
+                continue;
+            }
+            if (!in_array($eid, $idpWithACL[$l])) {
+                echo "[ERROR]   the mentioned IdP '" . $l . "' at SP '" . $eid . "' is not in the IdP ACL" . PHP_EOL;
+            }
+        }
+    }
 }
