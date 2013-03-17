606d90f6bcece077de49e69b0daeb345d2f3c420
François Kooman <fkooman@tuxed.net>
2013-03-17 19:25:23 +0100

    verify that a name is set, if not set it

--- docs/migrateJanus2.php
+++ docs/migrateJanus2.php
@@ -128,6 +128,9 @@ validateContacts($saml20_sp);
 validateEndpoints($saml20_idp);
 validateEndpoints($saml20_sp);
 
+enforceName($saml20_idp);
+enforceName($saml20_sp);
+
 if (FALSE === @file_put_contents($argv[1] . DIRECTORY_SEPARATOR . "saml20-idp-remote.json", json_encode(array_values($saml20_idp)))) {
     throw new Exception("unable to write 'saml20-idp-remote.json'");
 }
@@ -370,4 +373,26 @@ function filterEndpoint(array $ep)
     }
 
     return $validatedEndpoint;
+}
+
+function enforceName(&$entities)
+{
+    // name needs to be set, no matter what, set it to entityId if it is not specified
+    foreach ($entities as $eid => $metadata) {
+        if (array_key_exists("name", $metadata) && is_array($metadata['name']) && array_key_exists("en", $metadata["name"]) && !empty($metadata["name"]["en"])) {
+            // all is fine
+            continue;
+        }
+
+        if (array_key_exists("name", $metadata) && is_array($metadata['name']) && array_key_exists("nl", $metadata["name"]) && !empty($metadata["name"]["nl"])) {
+            // use name:nl for name:en
+            echo "[WARNING] no name:en set for '" . $entities[$eid]['metadata-set'] . "' entity '" . $eid . "', we use name:nl for name:en" . PHP_EOL;
+            $entities[$eid]['name']['en'] = $metadata['name']['nl'];
+            continue;
+        }
+
+        // no good name:en or name:nl available, set name:en to entityId
+        echo "[WARNING] no name set for '" . $entities[$eid]['metadata-set'] . "' entity '" . $eid . "', we use entityId for name:en" . PHP_EOL;
+        $entities[$eid]['name']['en'] = $metadata['entityid'];
+    }
 }
