07461179688106cab580814c701d51a53ed6ebd4
François Kooman <fkooman@tuxed.net>
2013-06-25 15:33:15 +0200

    update DB schema

--- schema/db.sql
+++ schema/db.sql
@@ -1,12 +1,8 @@
-CREATE TABLE IF NOT EXISTS metadata (
+CREATE TABLE metadata (
     metadata_set VARCHAR(32) NOT NULL,
     entity_id VARCHAR(255),
+    revision_id INTEGER DEFAULT 0,
+    revision_note TEXT DEFAULT NULL,
     entity_data TEXT NOT NULL,
-    UNIQUE (metadata_set , entity_id)
-);
-
-CREATE TABLE IF NOT EXISTS db_changelog (
-    patch_number INTEGER NOT NULL,
-    description TEXT NOT NULL,
-    PRIMARY KEY (patch_number)
+    UNIQUE (metadata_set , entity_id , revision_id)
 );
